9801ca6cd300caced98093f8489d1de6433bbc12
fetaoily <fetaoily@gmail.com>
2018-04-27 13:56:16 +0800

    Init Commit!

---
+++ index.js
@@ -0,0 +1,30 @@
+console.log("If module not found, install express globally `npm i express -g`!");
+// ##
+// require
+const Gun = require('gun');
+const express = require('express');
+
+// ##
+// Gun Server Port
+const port = process.env.OPENSHIFT_NODEJS_PORT || process.env.VCAP_APP_PORT || process.env.PORT || process.argv[2] || 8080;
+
+// ##
+// Config: express
+const app = express();
+app.use(Gun.serve);
+app.use(express.static(__dirname));
+app.use('node_modules', express.static('node_modules'));
+
+// ##
+// Config: Gun
+const server = app.listen(port);
+const gun = Gun({file: 'data.json', web: server});
+
+// ##
+setInterval(() => {
+  gun.get('heartbeat').put({
+    heartbeat: new Date().getTime()
+  });
+});
+
+console.log('Server started on port ' + port + ' with /gun');
